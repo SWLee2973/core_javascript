25187541979f816631388d5e7b39047b26fc583d
SWLee2973 <kek2973@naver.com>
2023-12-18 15:22:42 +0900

    [learn] module 프로그래밍

--- client/main.js
+++ client/main.js
@@ -1,4 +1,44 @@
-const a = 10;
-const b = 'hello';
+// 모듈 프로그래밍
 
-console.log(navigator.language);
+// //named export (이름 내보내기) -> { 이름 }
+// import { getNode } from "./lib/dom/getNode.js";
+// import { insertLast } from "./lib/dom/insert.js";
+// //default export (기본 내보내기) -> 이름
+// import clearContents from "./lib/dom/clear.js";
+
+import {getNode, clearContents, insertLast} from './lib/index.js';
+
+
+const first = getNode('#firstNumber');
+const second = getNode('#secondNumber');
+const clear = getNode('#clear');
+const result = getNode('.result');
+let total = 0;
+
+function handleCalculate() {
+
+  const firstValue = +first.value;
+  const secondValue = +second.value;
+  
+  if(typeof firstValue !== 'number' || 
+      typeof secondValue !== 'number') return;
+  total = firstValue + secondValue;
+
+  
+  // result.innerHTML = resultValue;
+  clearContents(result);
+  insertLast (result, total);
+
+}
+
+function handleClear(e) {
+  e.preventDefault();
+
+  clearContents(first);
+  clearContents(second);
+  clearContents(result);
+}
+
+first.addEventListener('input', handleCalculate);
+second.addEventListener('input', handleCalculate);
+clear.addEventListener('click', handleClear);
