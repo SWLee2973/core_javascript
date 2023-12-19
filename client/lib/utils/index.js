978278daef1ff16148f58936cd8bafc578e4978c
SWLee2973 <kek2973@naver.com>
2023-12-19 15:02:23 +0900

    [learn] random dice 완성

---
+++ client/lib/utils/index.js
@@ -0,0 +1,7 @@
+
+
+
+
+export * from './copy.js'
+export * from './typeOf.js'
+export * from './memo.js'
--- client/main.js
+++ client/main.js
@@ -1,4 +1,81 @@
-const a = 10;
-const b = 'hello';
 
-console.log(navigator.language);
+import { clearContents, diceAnimation, endScroll, getNode, getNodes, insertLast, memo } from './lib/index.js';
+
+const [rollingButton, recordButton, resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper');
+
+// [phase-1]
+// 1. 주사위 굴리기
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation이 실행되도록
+
+// toggle
+// 1. 주사위 눈 값 가져오기
+// 2. 태그 만들기
+// 3. 뿌려주기
+
+let count = 0;
+let total = 0;
+
+function createItem(value) {
+  const template = /* html */`
+  <tr>
+    <td>${++count}</td>
+    <td>${value}</td>
+    <td>${total += +value}</td>
+  </tr>
+  `
+
+  return template;
+}
+
+function renderRecordItem() {
+
+  const diceValue = memo("cube").dataset.dice;
+
+  insertLast('.recordList tbody', createItem(diceValue));
+  endScroll(recordListWrapper);
+}
+
+
+const handleRollingDice = (() => {
+  
+  let isClicked = false;
+  let stopAnimation;
+
+  return () => {
+    if(!isClicked) { // 실행
+      stopAnimation = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled = true;
+
+    } else { // 정지
+      clearInterval(stopAnimation)
+      recordButton.disabled = false;
+      resetButton.disabled = false;
+
+    }
+    
+    isClicked = !isClicked;
+  }
+  
+})();
+
+function handleRecord() {
+  recordListWrapper.hidden = false;
+
+  renderRecordItem();
+}
+
+function handleReset() {
+  recordListWrapper.hidden = true;
+
+  clearContents(getNode('tbody'));
+  count = 0;
+  total = 0;
+}
+
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+resetButton.addEventListener('click', handleReset);
+
+
